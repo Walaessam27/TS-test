ebe44d43c461d9b6264256fcef9062490ec3cc9a
Walaessam27 <145562141+Walaessam27@users.noreply.github.com>
2025-08-09 16:19:18 +0300

    first commit

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+console.log("Hello, World!");
+let firstname = "Alice";
+let age = 20;
+if (age < 100) {
+    age += 5;
+}
+console.log(age);
+
+function render(document) {
+    console.log(document);
+}   
+let numbers;
+numbers = [1, 2, 3, 4, 5];
+numbers[0] = 10;
+let user;
+user = ["Alice", 30];
+export {};
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+console.log("Hello, World!");
+let firstname: string = "Alice";
+let age: number = 20;
+
+if (age < 100) {
+    age += 5;
+}
+console.log(age);
+
+
+
+// function render(document: any)
+function render(document) {
+    console.log(document);
+}
+
+
+//let numbers: number[] = [1, 2, 3, 4, 5];
+let numbers: number[];
+numbers = [1, 2, 3, 4, 5];
+
+numbers[0] = 10;
+
+
+let user: [string, number];
+user = ["Alice", 30];
+
